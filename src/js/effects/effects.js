07a0e6cb5a94470ca0606aea392e3499fea08ee4
Shovkin <artishock4live@gmail.com>
2021-08-05 22:59:26 +0300

    added hover logic

--- src/js/effects/effects.js
+++ src/js/effects/effects.js
@@ -8,13 +8,39 @@ export function bindShowAnimationOnHover() {
             btnText.classList.add('element-hovered');
         })
 
-        el.addEventListener('mouseleave', function (){
+        el.addEventListener('mouseleave', function () {
             btnText.classList.remove('element-hovered');
         })
-    })
+    });
+
+    bindShowDescriptionOnHover();
 
 }
 
 export function bindShowDescriptionOnHover() {
 
+    const container = document.querySelector('.content__products .splide__content');
+    const cards = document.querySelectorAll('.content__products li');
+
+    cards.forEach(card => {
+
+        const description = card.querySelector('.card__product--description');
+
+        card.addEventListener('mouseenter', () => {
+
+            description.getBoundingClientRect().left - container.getBoundingClientRect().left < 0
+                ? description.style.left = '0'
+                : description.style.left = 'auto'
+
+            container.getBoundingClientRect().right - description.getBoundingClientRect().right < 0
+                ? description.style.right = '0'
+                : description.style.right = 'auto'
+
+        });
+
+        card.addEventListener('mouseleave', () => {
+            description.style.left = 'auto';
+            description.style.right = 'auto';
+        });
+    })
 }
