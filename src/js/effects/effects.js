756308ab8390e884e3d6dd136571d3d65acb4a5d
Artem Shovkin <artishock4live@gmail.com>
2021-08-06 11:16:56 +0300

    fixed positioning

--- src/js/effects/effects.js
+++ src/js/effects/effects.js
@@ -20,21 +20,23 @@ export function bindShowAnimationOnHover() {
 export function bindShowDescriptionOnHover() {
 
     const container = document.querySelector('.content__products .splide__content');
-    const cards = document.querySelectorAll('.content__products li');
+    const cards = document.querySelectorAll('.content__products .card.card__product');
 
     cards.forEach(card => {
 
         const description = card.querySelector('.card__product--description');
 
-        card.addEventListener('mouseenter', () => {
+        card.addEventListener('mouseenter', (e) => {
 
-            description.getBoundingClientRect().left - container.getBoundingClientRect().left < 0
-                ? description.style.left = '0'
-                : description.style.left = 'auto'
+            if (description.getBoundingClientRect().left - container.getBoundingClientRect().left < 0) {
+                description.style.left = '0';
+            } else if (container.getBoundingClientRect().right - description.getBoundingClientRect().right <= 0) {
+                description.style.right = '0';
+            }
 
-            container.getBoundingClientRect().right - description.getBoundingClientRect().right < 0
-                ? description.style.right = '0'
-                : description.style.right = 'auto'
+            console.log('description', description.getBoundingClientRect())
+            console.log('container', container.getBoundingClientRect())
+
 
         });
 
