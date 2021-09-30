07d6d451b33b878b9c871d6d7f651c5e610e3259
Shovkin <artishock4live@gmail.com>
2021-09-30 21:58:26 +0300

    added docker-compose confs

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const config = require('config');
+const path = require('path');
+const app = express();
+
+const PORT = config.get('port');
+
+if(process.env.NODE_ENV === 'production') {
+    app.use('/', express.static(path.join(__dirname, 'lemonade', 'dist')));
+
+    app.get('*', (req, res) => {
+            res.sendFile(path.resolve(__dirname, 'lemonade', 'dist', 'index.html'))
+    });
+}
+
+app.listen(PORT, () => console.log('some request'));
