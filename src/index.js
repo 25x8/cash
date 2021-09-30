5283e28e95ff2250c984856d5e62068e2e2a1efb
Artem Shovkin <artishock4live@gmail.com>
2021-09-30 17:58:19 +0300

    added content

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ import  './scss/style.scss';
 import {initProductsSplide, initCertificatesSplide} from "./js/modules/splidejs";
 import {initValidation} from "./js/validation/validation";
 import {bindShowAnimationOnHover} from "./js/effects/effects";
+import {initSwiper} from "./js/modules/swiperjs";
 
 document.body.onload = () => {
-    initProductsSplide('.content__products .splide');
-    initCertificatesSplide('.content__certificates .splide');
+    initSwiper();
     initValidation();
     bindShowAnimationOnHover();
 };
---
+++ src/js/modules/swiperjs.js
@@ -0,0 +1,19 @@
+import Swiper from 'swiper';
+// import Swiper styles
+import 'swiper/css';
+
+export function initSwiper() {
+
+    const swiper = new Swiper('.swiper', {
+        slidesPerView: 'auto',
+    });
+
+    new Swiper('.swiper-card-account', {
+        slidesPerView: 'auto',
+
+    });
+
+    new Swiper('.swiper-card-compact', {
+        slidesPerView: 'auto',
+    });
+}
